0911f5f31fec5c0466aac2051fecf17a41eb2161
Radrei <purpleyasha@yahoo.com>
2014-01-29 12:21:03 -0500

    Initial Commit
    initial modal plugin commit

---
+++ modal/modal2.js
@@ -0,0 +1,38 @@
+(function($){
+    $.fn.modal = function() {
+		$('a').click(function(e) {
+			$("#mask").fadeIn(700);
+			if ($(this).hasClass('fade')) {
+				$("#popup").fadeIn(700);
+			}
+			else if ($(this).hasClass('ttb')) {
+				$("#popupTTB").animate({top: '200px'}, 1000);
+			}
+			else if ($(this).hasClass('btt')) {
+				$("#popupBTT").animate({bottom: '500px'}, 1000);
+			}
+			else if ($(this).hasClass('ltr')) {
+				$("#popupLTR").animate({left: '600px'});
+			}
+			else if ($(this).hasClass('rtl')) {
+				$("#popupRTL").animate({left: '450px'});
+			}
+			else if ($(this).hasClass('close')) {
+				$(".popup").fadeOut();
+				$("#mask").hide();
+			}
+		});
+	};
+})(jQuery);
+/*
+$("#popup-fi").slideDown('slow');
+
+
+		
+		$('#mask').fadeIn(1000);
+		
+		$("#close").click(function(){
+			$("#popup-fi").hide();
+			$("#mask").hide();
+		});
+*/
